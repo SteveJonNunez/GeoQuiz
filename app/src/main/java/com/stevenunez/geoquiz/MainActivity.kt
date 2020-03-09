a9cf3d934251af37c5c2c97a9c18fe3a8633a608
SteveNunez <runningtechy@gmail.com>
2020-03-09 09:25:52 -0400

    Wired up the cheat button.

--- app/src/main/java/com/stevenunez/geoquiz/MainActivity.kt
+++ app/src/main/java/com/stevenunez/geoquiz/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.stevenunez.geoquiz
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -17,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
 
     private val quizViewModel: QuizViewModel by lazy {
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {view: View ->
@@ -45,6 +46,9 @@ class MainActivity : AppCompatActivity() {
         }
         nextButton.setOnClickListener {view: View ->
             nextQuestion()
+        }
+        cheatButton.setOnClickListener { view: View ->
+            cheat()
         }
 
         setQuestion()
@@ -91,6 +95,10 @@ class MainActivity : AppCompatActivity() {
                 R.string.incorrect_toast
 
         Toast.makeText(this, messageResourceId, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun cheat() {
+
     }
 
     private fun nextQuestion() {
