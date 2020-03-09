b7d27ce2062fc0e7efbb9b301a3f0563bce9e8a4
SteveNunez <runningtechy@gmail.com>
2020-03-08 22:42:25 -0400

    Added the view model to the activity

--- app/src/main/java/com/stevenunez/geoquiz/MainActivity.kt
+++ app/src/main/java/com/stevenunez/geoquiz/MainActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
     private final val TAG = "MainActivity"
@@ -31,6 +33,10 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate(Bundle?) called")
 
         setContentView(R.layout.activity_main)
+
+        val provider: ViewModelProvider = ViewModelProviders.of(this)
+        val quizViewModel = provider.get(QuizViewModel::class.java)
+        Log.d(TAG, "Got a QuizViewModel: $quizViewModel")
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
