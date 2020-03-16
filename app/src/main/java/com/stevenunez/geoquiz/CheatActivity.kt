cc25b984bc73f79afeeb1f4963580d08e4cffc1c
SteveNunez <runningtechy@gmail.com>
2020-03-15 22:17:39 -0400

    Implemented cheat button on CheatActivity

--- app/src/main/java/com/stevenunez/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/stevenunez/geoquiz/CheatActivity.kt
@@ -2,12 +2,17 @@ package com.stevenunez.geoquiz
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 private const val EXTRA_ANSWER_IS_TRUE = "com.stevenunez.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
+    private lateinit var answerTextView: TextView
+    private lateinit var showAnswerButton: Button
+
     private var answerIsTrue = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,17 @@ class CheatActivity : AppCompatActivity() {
         setContentView(R.layout.activity_cheat)
 
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+
+        showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            answerTextView.setText(answerText)
+        }
     }
 
     companion object {
